1a3260f7bf8f14a87ac1eb61c24da4e25fb98808
Lucas Jaud <lucasjaud19@gmail.com>
2025-05-15 09:46:46 -0300

    Feat: Estrutura inicial do sistema

---
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/AmantesCafezinhoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.academico.Amantes_Cafezinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmantesCafezinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmantesCafezinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/academico/Amantes_Cafezinho/AmantesCafezinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.academico.Amantes_Cafezinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmantesCafezinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

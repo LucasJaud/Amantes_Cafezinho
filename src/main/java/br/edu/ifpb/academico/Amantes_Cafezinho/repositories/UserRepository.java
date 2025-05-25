157e8e5bdc3495dc990fbb9742c6bfdc72142303
¨filipe-rds¨ <filipe.rds.dev@gmail.com>
2025-05-25 16:59:04 -0300

    feat: processamento de login baseado no tipo de usuário

---
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.academico.Amantes_Cafezinho.repositories;
+
+import br.edu.ifpb.academico.Amantes_Cafezinho.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}

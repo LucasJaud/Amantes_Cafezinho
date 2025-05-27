4a7781bc159bbea8ad94226aac9a4f58344008d4
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2025-05-27 16:49:18 -0300

    feat: review creation form
    Task ID: 18078

--- src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/services/FachadaService.java
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/services/FachadaService.java
@@ -2,13 +2,11 @@ package br.edu.ifpb.academico.Amantes_Cafezinho.services;
 
 import java.util.List;
 
-import br.edu.ifpb.academico.Amantes_Cafezinho.models.Review;
+import br.edu.ifpb.academico.Amantes_Cafezinho.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifpb.academico.Amantes_Cafezinho.errors.CafeteriaSemUnidades;
-import br.edu.ifpb.academico.Amantes_Cafezinho.models.Cafeteria;
-import br.edu.ifpb.academico.Amantes_Cafezinho.models.Unit;
 
 @Service
 public class FachadaService {
@@ -21,6 +19,9 @@ public class FachadaService {
 
     @Autowired
     private ReviewService reviewService;
+
+    @Autowired
+    private ReviewerService reviewerService;
 
 
     public Unit criarUnidade(Unit unidade) {
@@ -58,5 +59,9 @@ public class FachadaService {
 
     public Review criarAvaliacao(Review review) {
         return reviewService.criarAvaliacao(review);
+    }
+
+    public Reviewer buscarReviewerPorUser(User user) {
+        return reviewerService.buscarPorUser(user);
     }
 }

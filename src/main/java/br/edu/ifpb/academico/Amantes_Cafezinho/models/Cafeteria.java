3a7a7b6a7059178fc3c572302308b6d4b91f6eab
Gabriel Félix <gfedacs@hotmail.com>
2025-05-28 00:04:21 -0300

    [AC-18105]-feat: Criação de 3 novas features
    - Modificações:
    
    -- Foi criado a feat pra cadastrar uma nova unidade.
    
    -- Listar todas as unidades atreladadas a uma cafeteria.
    
    -- Acessar o perfil de uma unidade.

--- src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/models/Cafeteria.java
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/models/Cafeteria.java
@@ -30,6 +30,6 @@ public class Cafeteria{
 
     private String fantasyName;
 
-    @OneToMany(mappedBy = "cafeteria", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cafeteria",fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<Unit> units;
 }

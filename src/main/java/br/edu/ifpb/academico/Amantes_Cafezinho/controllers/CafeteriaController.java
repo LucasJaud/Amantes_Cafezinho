eae09aa609ba9586cb0484e5e61d7e4478eb6d77
Lucas Jaud <lucasjaud19@gmail.com>
2025-05-28 20:09:56 -0300

    feat: implementação parcial do upload de photos

--- src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/controllers/CafeteriaController.java
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/controllers/CafeteriaController.java
@@ -1,7 +1,10 @@
 package br.edu.ifpb.academico.Amantes_Cafezinho.controllers;
 
+import br.edu.ifpb.academico.Amantes_Cafezinho.errors.UserNotFound;
 import br.edu.ifpb.academico.Amantes_Cafezinho.models.Cafeteria;
+import br.edu.ifpb.academico.Amantes_Cafezinho.models.User;
 import br.edu.ifpb.academico.Amantes_Cafezinho.repositories.CafeteriaRepository;
+import br.edu.ifpb.academico.Amantes_Cafezinho.repositories.UserRepository;
 import br.edu.ifpb.academico.Amantes_Cafezinho.services.CafeteriaService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/cafeteria")
@@ -18,11 +29,21 @@ public class CafeteriaController {
     @Autowired
     private CafeteriaService service;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/{cnpj}")
     public ModelAndView cafeteriaView(ModelAndView mav,@PathVariable String cnpj) {
         Cafeteria cafeteria = service.resgatarCafeteriaPorCNPJ(cnpj);
         mav.addObject("cafeteria", cafeteria);
         mav.setViewName("views/showCafeteria");
         return mav;
+    }
+
+    public String savePhoto(@PathVariable Long id, @RequestParam MultipartFile file) throws IOException {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFound(id));
+        Cafeteria cafeteria = service.findCafeteriaByUser(user);
+        service.savePhoto(cafeteria, file);
+        return "redirect:/cafeteria/"+cafeteria.getCNPJ();
     }
 }

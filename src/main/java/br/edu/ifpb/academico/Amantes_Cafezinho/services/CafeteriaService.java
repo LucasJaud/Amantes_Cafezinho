96390b1ab2c0670396e57809576fc409bdb3ea11
Lucas Jaud <lucasjaud19@gmail.com>
2025-05-28 00:02:45 -0300

    Refactor: alteração no metodo do service de cafeteria, alteração no atributo cnpj do modelo cafeteria

--- src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/services/CafeteriaService.java
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/services/CafeteriaService.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.academico.Amantes_Cafezinho.services;
 
+import br.edu.ifpb.academico.Amantes_Cafezinho.errors.CafeteriaNotFoundException;
+import br.edu.ifpb.academico.Amantes_Cafezinho.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,11 @@ public class CafeteriaService {
     private CafeteriaRepository cafeteriaRepository;
 
     public Cafeteria resgatarCafeteriaPorCNPJ(String cnpj) {
+        return cafeteriaRepository.findByCNPJ(cnpj).orElseThrow(() -> new CafeteriaNotFoundException(cnpj));
+    }
 
-        return cafeteriaRepository.findByCNPJ(cnpj).get();
-
+    public Cafeteria findCafeteriaByUser(User user) {
+        return cafeteriaRepository.findByUser(user).orElse(null);
     }
     
 }

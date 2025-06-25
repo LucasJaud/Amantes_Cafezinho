8a21699300c29aecaa18e4f09c32afd9674eff85
¨filipe-rds¨ <filipe.rds.dev@gmail.com>
2025-06-25 20:10:29 -0300

    feat: adicionando suporte a média de avaliações

--- src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/services/ReviewService.java
+++ src/main/java/br/edu/ifpb/academico/Amantes_Cafezinho/services/ReviewService.java
@@ -9,6 +9,7 @@ import br.edu.ifpb.academico.Amantes_Cafezinho.models.Status;
 import br.edu.ifpb.academico.Amantes_Cafezinho.models.Unit;
 import br.edu.ifpb.academico.Amantes_Cafezinho.repositories.ReviewRepository;
 import br.edu.ifpb.academico.Amantes_Cafezinho.repositories.StatusRepository;
+import br.edu.ifpb.academico.Amantes_Cafezinho.repositories.UnitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,8 +25,40 @@ public class ReviewService {
     @Autowired
     private StatusRepository statusRepository;
 
+    @Autowired
+    private UnitRepository unitRepository;
+
     public Review criarReview(Review review) {
-        return reviewRepository.save(review);
+        Review reviewGerada = reviewRepository.save(review);
+        Unit unidade = reviewGerada.getUnit();
+        unidade.setAverage(unitRepository.findAverageRatingByUnitId(unidade.getId()));
+        unitRepository.save(unidade);
+
+        return reviewGerada;
+    }
+
+    public void excluirReview(Long id){
+        if (reviewRepository.existsById(id)) {
+            Review reviewDeletada = this.buscarPorId(id);
+            reviewRepository.deleteById(id);
+            Unit unidade = reviewDeletada.getUnit();
+            unidade.setAverage(unitRepository.findAverageRatingByUnitId(unidade.getId()));
+            unitRepository.save(unidade);
+        }
+
+    }
+
+    public Review atualizarReview(Long id, Review novaAvaliacao){
+        Review original = reviewRepository.findById(id)
+                .orElseThrow(() -> new ReviewNotFoundException("Avaliação com ID " + id + " não encontrada!"));
+
+        original.update(novaAvaliacao);
+        Review reviewAtualizada = reviewRepository.save(original);
+        Unit unidade = reviewAtualizada.getUnit();
+        unidade.setAverage(unitRepository.findAverageRatingByUnitId(unidade.getId()));
+        unitRepository.save(unidade);
+
+        return reviewAtualizada;
     }
 
     public Review buscarPorId(Long id) {
@@ -47,18 +80,18 @@ public class ReviewService {
         return reviews.stream().map(ReviewListDTO::fromEntity).toList();
     }
 
-    public void excluirReview(Long id) {
-        if (reviewRepository.existsById(id)) {
-            reviewRepository.deleteById(id);
-        }
-    }
-
-    public void atualizarReview(Long id, Review novaAvaliacao){
-        Review original = reviewRepository.findById(id)
-                .orElseThrow(() -> new ReviewNotFoundException("Avaliação com ID " + id + " não encontrada!"));
-
-        original.update(novaAvaliacao);
-        reviewRepository.save(original);
-    }
+//    public void excluirReview(Long id) {
+//        if (reviewRepository.existsById(id)) {
+//            reviewRepository.deleteById(id);
+//        }
+//    }
+//
+//    public void atualizarReview(Long id, Review novaAvaliacao){
+//        Review original = reviewRepository.findById(id)
+//                .orElseThrow(() -> new ReviewNotFoundException("Avaliação com ID " + id + " não encontrada!"));
+//
+//        original.update(novaAvaliacao);
+//        reviewRepository.save(original);
+//    }
 
 }
